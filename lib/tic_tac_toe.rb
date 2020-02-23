8bd668581c76357f0e6595566e9314739033eb67
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-02-23 01:59:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,3 +38,15 @@ def valid_move?(board, index)
     true
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, "X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
