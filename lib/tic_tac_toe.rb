e0648bdd9c6216ebd9656d95c74b09c726e041b6
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-02-23 02:10:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,4 +95,9 @@ def play(board)
   until over?(board)
     turn(board)
   end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
 end
