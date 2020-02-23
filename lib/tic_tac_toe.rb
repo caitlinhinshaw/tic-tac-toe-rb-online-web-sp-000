120ac2ae257da0659e6a405e6625af461d946df8
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-02-23 01:59:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,3 +50,22 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  occupied_spaces = 0
+  board.each do |space|
+    if space === "X" || space === "O"
+      occupied_spaces += 1
+    end
+  end
+  occupied_spaces
+end
+
+def current_player(board)
+  # if turn_count(board).even?
+  #   "X"
+  # else
+  #   "O"
+  # end
+  turn_count(board).even? ? "X" : "O"
+end
